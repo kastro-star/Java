d1481234eea91d58af66fe12a789c61c8eab871d
Kastro .R <kastrouchiha@gmail.com>
2025-08-07 13:38:28 -1200

    Add files via upload

---
+++ OOPS CONCEPT/OOPS_17_Exceptionhandling.java
@@ -0,0 +1,14 @@
+public class OOPS_17_Exceptionhandling {
+    public static void main(String[] args) {
+        int a = 5;
+        int c = 0;
+        try{
+            System.out.println(a/c);
+        }
+        catch (Exception l ){
+            c= 5;
+            System.out.println("kastro  the output is creating error ");
+
+        }
+    }
+}
